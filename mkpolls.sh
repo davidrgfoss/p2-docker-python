17a3d575f4dd68b0b07beee53f720e0c5143e9d7
davidrg <davidrg@davidrgfoss.com>
2023-06-18 03:11:14 +0200

    Practica 2

--- mkpolls.sh
+++ mkpolls.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+wget https://raw.githubusercontent.com/vishnubob/wait-for-it/master/wait-for-it.sh
+chmod +x wait-for-it.sh
+
+# Espera hasta que MariaDB esté lista para aceptar conexiones.
+./wait-for-it.sh $HOST:3306 --timeout=30 --strict -- echo "MariaDB ok"
+
 python3 django_tutorial/manage.py makemigrations
 python3 django_tutorial/manage.py migrate
 python3 django_tutorial/manage.py createsuperuser --noinput
